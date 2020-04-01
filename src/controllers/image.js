a11cc78e2e8bd6b7c4a6808681273cd52e59351b
Elias Guere <eliasguere1025@gmail.com>
2020-04-01 00:01:25 -0500

    Creando rutas para el tratado de imagenes

---
+++ src/controllers/image.js
@@ -0,0 +1,23 @@
+const ctrl = {};
+
+ctrl.index = (req, res) => {
+    res.send('Hi');
+};
+
+ctrl.create = (req, res) => {
+    res.send('Hi');
+};
+
+ctrl.like = (req, res) => {
+    res.send('Hi');
+};
+
+ctrl.comment = (req, res) => {
+    res.send('Hi');
+};
+
+ctrl.remove = (req, res) => {
+    res.send('Hi');
+};
+
+module.exports = ctrl;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,15 @@
+const router = require('express').Router();
+
+const HomeController = require('../controllers/home');
+const ImageController = require('../controllers/image');
+
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.send('Index page');
-  });
+    router.get('/', HomeController.index);
+    router.get('/images/:image_id', ImageController.index);
+    router.post('/images', ImageController.create);
+    router.post('/images/:image_id/like', ImageController.like);
+    router.post('/images/:image_id/comment', ImageController.comment);
+    router.delete('/images/:image_id', ImageController.remove);
+
+    app.use(router);
 };
