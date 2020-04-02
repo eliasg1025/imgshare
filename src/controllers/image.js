403f6951d269462a2cb1e59f555729ecd6202b45
Elias Guere <eliasguere1025@gmail.com>
2020-04-01 19:13:27 -0500

    Implementando subida de imagenes

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,25 @@
+const path = require('path');
+const { randomNumber } = require('../helpers/libs');
+const fs = require('fs-extra');
+
 const ctrl = {};
 
 ctrl.index = (req, res) => {
     res.send('Hi');
 };
 
-ctrl.create = (req, res) => {
-    res.send('Hi');
+ctrl.create = async (req, res) => {
+    const imgUrl = randomNumber();
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLocaleLowerCase();
+    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+
+    const validExt = ['.png', '.jpg', '.jpeg', '.gif'];
+    if (validExt.includes(ext)) {
+        await fs.rename(imageTempPath, targetPath);
+    }
+
+    res.send('works');
 };
 
 ctrl.like = (req, res) => {
