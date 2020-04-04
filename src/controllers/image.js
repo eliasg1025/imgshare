f4ec3b17d2cfb06ea570e02df64e63c15c7e6ce3
Elias Guere <eliasguere1025@gmail.com>
2020-04-04 01:00:27 -0500

    Eliminando las imagenes y sus comentarios

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -58,8 +58,7 @@ ctrl.create = (req, res) => {
                     description: req.body.description
                 });
                 const imageSaved = await newImg.save();
-                //res.redirect('/images');
-                res.send('works');
+                res.redirect(`/images/${imageSaved.uniqueId}`);
             } else {
                 await fs.unlink(imageTempPath);
                 res.status(400).json({ error: 'Only images are allowed' });
@@ -105,8 +104,17 @@ ctrl.comment = async (req, res) => {
     }
 };
 
-ctrl.remove = (req, res) => {
-    res.send('Hi');
+ctrl.remove = async (req, res) => {
+    const image = await Image.findOne({
+        filename: { $regex: req.params.image_id }
+    });
+
+    if (image) {
+        await fs.unlink(path.resolve(`./src/public/upload/${image.filename}`));
+        await Comment.deleteOne({ image_id: image._id });
+        await image.remove();
+        res.json(true);
+    }
 };
 
 module.exports = ctrl;
--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,3 +1,10 @@
+$('#post-comment').hide();
+
+$('#btn-toggle-comment').click(e => {
+    e.preventDefault();
+    $('#post-comment').slideToggle();
+});
+
 $('#btn-like').click(function (e) {
     e.preventDefault();
     let image_id = $(this).data('id');
@@ -9,4 +16,34 @@ $('#btn-like').click(function (e) {
         .fail(err => {
             console.log(err);
         });
+});
+
+$('#btn-delete').click(function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    const response = confirm('Are you sure you want to delete this image?');
+
+    if (response) {
+        const image_id = $this.data('id');
+
+        $.ajax({
+            url: `/images/${image_id}`,
+            type: 'DELETE'
+        })
+            .done(function (result) {
+                $this.removeClass('btn-danger').addClass('btn-success');
+                $this.find('i').removeClass('fa-times').addClass('fa-check');
+                /*html*/
+                $this.append(`
+                    <span>Deleted!</span>
+                `);
+
+                setTimeout(() => {
+                    window.location.href = '/';
+                }, 2000);
+            })
+            .fail(function (err) {
+                console.log(err);
+            });
+    }
 });
